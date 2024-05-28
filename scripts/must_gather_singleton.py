de7d23463b57bf2d6b20eed86f35032aee4e597f
Mihai Idu <idumihai16@gmail.com>
2024-05-28 14:34:54 +0200

    Update must_gather_singleton.py

--- scripts/must_gather_singleton.py
+++ scripts/must_gather_singleton.py
@@ -400,7 +400,7 @@ def invoke_must_gather(output_list = None, bundle_must_gather = None, debug = Fa
       # If both output_list and bundle_must_gather are empty, invoke with default parameters.
       # This ensures that all the available means of collections are performed.
       oc.invoke('adm', ['must-gather', '--',
-              '--node-name=' + node_name,
+              '--node-name=', node_name,
               '/usr/bin/gather && /usr/bin/gather_audit_logs',
               '--image-stream=openshift/must-gather'])
     else:
@@ -408,7 +408,7 @@ def invoke_must_gather(output_list = None, bundle_must_gather = None, debug = Fa
         print("Calling found must gather")
       # Otherwise, invoke with specified output_list and bundle_must_gather
       oc.invoke('adm', ['must-gather', '--',
-              '--node-name=' + node_name,
+              '--node-name=', node_name,
               '/usr/bin/gather && /usr/bin/gather_audit_logs',
               '--image-stream=openshift/must-gather',
               set(output_list),
