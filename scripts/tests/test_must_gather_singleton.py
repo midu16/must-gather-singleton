c368e1dbe1197a84ffb09df39b9143be2d201bd9
Mihai Idu <idumihai16@gmail.com>
2024-05-28 11:12:00 +0200

    Update test_must_gather_singleton.py

--- scripts/tests/test_must_gather_singleton.py
+++ scripts/tests/test_must_gather_singleton.py
@@ -1,6 +1,7 @@
 # tests/test_must_gather_singleton.py
 
 import pytest
+from unittest.mock import patch
 from must_gather_singleton import (
     check_kube_config,
     get_csv_related_images_with_keyword,
@@ -24,10 +25,9 @@ def test_check_debug(debug, expected):
     ("/apps/must-gather/", "/apps/must-gather/"),
     ("/invalid/path", None)
 ])
-def test_validate_directory_path(directory_path, expected):
+@patch('must_gather_singleton.os.path.exists', side_effect=lambda x: x == "/apps/must-gather/")
+def test_validate_directory_path(mock_exists, directory_path, expected):
   assert validate_directory_path(directory_path) == expected
-
-# Mocking Kubernetes API calls for remaining tests.
 
 def test_check_kube_config():
   assert check_kube_config()[0] is True
@@ -43,12 +43,15 @@ def test_newest_file_in_current_path():
   assert isinstance(newest_file_in_current_path(), tuple)
 
 @pytest.mark.parametrize("directory_path, tarfile_name", [
-    ("/test/path", "test.tar.gz"),
-    ("/invalid/path", "test.tar.gz")
+("/test/path", "test.tar.gz"),
+("/invalid/path", "test.tar.gz")
 ])
-def test_create_tar(directory_path, tarfile_name):
-  retval, _ = create_tar(directory_path, tarfile_name)
-  assert retval is True
+@patch('must_gather_singleton.os.path.exists', side_effect=lambda x: x == "/test/path")
+@patch('must_gather_singleton.tarfile.open')
+def test_create_tar(mock_tarfile_open, mock_exists, directory_path, tarfile_name):
+    mock_tar = mock_tarfile_open.return_value.__enter__.return_value
+    retval, _ = create_tar(directory_path, tarfile_name)
+    assert retval is True
 
 def test_operator_info():
   input_string = "lvms-operator.v4.14"
@@ -57,5 +60,6 @@ def test_operator_info():
 
 def test_get_must_gather_url():
   info = {'operator_name': 'lvms-operator',
-    'operator_major_version': '4', 'operator_version': '4.14'}
+  'operator_major_version': '4', 'operator_version': '4.14'}
   assert get_must_gather_url(info) == 'registry.redhat.io/lvms4/lvms-must-gather-rhel9:v4.14'
+
